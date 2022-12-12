270c3159c05473bf81b66921a7964385a66c2d53
Daniel0110000 <cariasdaniel261@mail.com>
2022-12-12 10:10:22 -0600

    Bottom navigation settings

---
+++ app/src/main/java/com/example/movieapp/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.movieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.movieapp.R
+import com.example.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+
+    }
+
+    private fun initBottomNavigation(){
+        val navController = findNavController(R.id.fragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+}
